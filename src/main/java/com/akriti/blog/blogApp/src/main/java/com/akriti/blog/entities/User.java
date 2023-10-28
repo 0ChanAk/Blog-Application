fd960f4254c5394f96c5ba3a3f9442c4d2c3b0c7
Chandana Akriti <chandanaakriti9@gmail.com>
2023-10-28 11:56:18 +0530

    updated

---
+++ src/main/java/com/akriti/blog/blogApp/src/main/java/com/akriti/blog/entities/User.java
@@ -0,0 +1,19 @@
+package com.akriti.blog.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "user_name", nullable = false, length = 100)
+    private String name;
+    private String email;
+    private String password;
+    private String about;
+
+}
